be03bcb729bb07a90b0a242fa5f52b62d17d15f5
bluemurder <bluemurder@engineer.com>
2017-11-03 10:32:49 +0100

    Added one shot temperature conversion example

---
+++ examples/ReadTemperatureOneShot/ReadTemperatureOneShot.ino
@@ -0,0 +1,47 @@
+/*****************************************************************************
+Arduino Library for Maxim DS1624 temperature sensor and memory
+
+MIT License
+
+Copyright (c) 2017 Alessio Leoncini
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*****************************************************************************/
+
+#include <DS1624.h>
+
+// Sensor presents all address pins connected to ground, and it will work in 
+// one-shot temperature conversion mode.
+DS1624 ds1624(false, false, false, false);
+
+void setup()
+{  
+  // Begin serial connection at 9600 baud
+  Serial.begin(9600);
+} 
+
+void loop()
+{
+  // Get current temperature
+  float temperature = ds1624.GetTemperature();
+  // Print it
+  Serial.println(temperature);
+  // Wait a while
+  delay(1000);
+}
